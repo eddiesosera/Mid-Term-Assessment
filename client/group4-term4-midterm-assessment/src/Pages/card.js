41385ea65b1476b4e0b830b67e27972593acfb1d
Eddie Sosera <21100419@virtualwindow.co.za>
2023-10-11 18:00:19 +0200

    View Products

--- client/group4-term4-midterm-assessment/src/Pages/card.js
+++ client/group4-term4-midterm-assessment/src/Pages/card.js
@@ -5,25 +5,25 @@ import { CardForm } from "./cardForm";
 
 function Card({ product }) {
     // const { addToCart } = useCart();
-    // const [isEditing, setIsEditing] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
     // const [editedProduct, setEditedProduct] = useState({ ...product });
 
-    // const handleBuyClick = () => {
-    //     // addToCart(product);
-    // };
-
-    // const handleUpdateClick = () => {
-    //     setIsEditing(true);
-    // };
-
-    const handleSaveClick = () => {
-        // Send a PUT request to update the product in the database
-        // axios.put(`/api/products/${product.productId}`, editedProduct).then((response) => {
-        //     // Update the product details with the edited product
-        //     setEditedProduct({ ...response.data });
-        //     setIsEditing(false);
-        // });
+    const handleBuyClick = () => {
+        // addToCart(product);
     };
+
+    const handleUpdateClick = () => {
+        setIsEditing(true);
+    };
+
+    // const handleSaveClick = () => {
+    //     // Send a PUT request to update the product in the database
+    //     axios.put(`/api/products/${product.productId}`, editedProduct).then((response) => {
+    //         // Update the product details with the edited product
+    //         setEditedProduct({ ...response.data });
+    //         setIsEditing(false);
+    //     });
+    // };
 
     // const handleInputChange = (event) => {
     //     const { name, value } = event.target;
@@ -34,22 +34,22 @@ function Card({ product }) {
     // };
 
     return (
-        <div>
-            {/* <h3>{isEditing ? <input type="text" name="name" value={editedProduct.name} onChange={handleInputChange} /> : editedProduct.name}</h3>
-            <p>Car Make: {isEditing ? <input type="text" name="carMake" value={editedProduct.carMake} onChange={handleInputChange} /> : editedProduct.carMake}</p>
-            <p>Car Model: {isEditing ? <input type="text" name="carModel" value={editedProduct.carModel} onChange={handleInputChange} /> : editedProduct.carModel}</p>
-            <p>Chassis Number: {isEditing ? <input type="text" name="chassisNumber" value={editedProduct.chassisNumber} onChange={handleInputChange} /> : editedProduct.chassisNumber}</p>
-            <p>Year: {isEditing ? <input type="text" name="year" value={editedProduct.year} onChange={handleInputChange} /> : editedProduct.year}</p>
-            <p>Part ID: {isEditing ? <input type="text" name="partId" value={editedProduct.partId} onChange={handleInputChange} /> : editedProduct.partId}</p> */}
+        <div style={{ display: 'flex', gap: '20px' }}>
 
-            {/* {isEditing ? (
-                <button onClick={handleSaveClick}>Save</button>
-            ) : (
-                <button onClick={handleUpdateClick}>Update</button>
-            )} */}
-            {/* <button onClick={handleBuyClick}>Buy</button> */}
+            <img style={{ width: '50px', height: '50px' }} src={product?.image} />
+            <h3>{product?.name}</h3>
+            <p>Car Make: {product?.carMake}</p>
+            <p>Car Model: {product?.carModel}</p>
+            <p>Chassis Number: {product?.chassisNumber}</p>
+            <p>Year: {product?.year}</p>
+            <p>Part ID: {product?.partId}</p>
 
-            <CardForm />
+            {/* {isEditing ? ( */}
+            {/* <button onClick={handleSaveClick}>Save</button> */}
+            {/* ) : ( */}
+            <button onClick={e => handleUpdateClick()}>Update</button>
+            {/* )} */}
+            <button onClick={e => handleBuyClick()}>Buy</button>
         </div>
     );
 }
